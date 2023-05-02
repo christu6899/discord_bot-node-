2fc5a7c1d14025d430616d742c1eae011a3c01ca
Chris Tu <worktu6899@gmail.com>
2023-05-02 20:33:58 +0800

    EP8 draw新增單抽選項

--- src/commands/draw/index.js
+++ src/commands/draw/index.js
@@ -4,6 +4,22 @@ import random from "random";
 export const command = new SlashCommandBuilder()
   .setName("抽卡")
   .setDescription("抽卡")
+  .addNumberOption((option) =>
+    option
+      .setName("抽數")
+      .setDescription("單抽或十連抽")
+      .setRequired(true)
+      .addChoices(
+        {
+          name: "單抽",
+          value: 1,
+        },
+        {
+          name: "十連抽",
+          value: 10,
+        }
+      )
+  )
   .addStringOption((option) =>
     option.setName("wish").setDescription("許願事項")
   );
@@ -12,8 +28,9 @@ export const action = async (ctx) => {
   const draw_Channel = await ctx.client.channels.fetch(
     process.env.DRAW_CHANNELID
   );
+  const times = ctx.options.get("抽數").value;
   const result = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < times; i++) {
     const random_number = random.int(1, 100);
     if (random_number === 1) {
       result.push("<:RBAngry:1102620068883468380>");
@@ -24,7 +41,10 @@ export const action = async (ctx) => {
     }
   }
 
-  if (result.every((item) => item === "<:BWAngry:1102620116870508554>")) {
+  if (
+    result.every((item) => item === "<:BWAngry:1102620116870508554>") &
+    (times == 10)
+  ) {
     result[9] = "<:emoji27:822849289792913409>";
   }
   let optionValue = ctx.options.get("wish");
