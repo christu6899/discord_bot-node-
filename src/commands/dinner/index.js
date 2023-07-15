e9935bb7f1c5e879def9d038ebfd0343cc89619e
Chris Tu <worktu6899@gmail.com>
2023-07-15 21:20:23 +0800

    EP12 新增晚餐選項

--- src/commands/dinner/index.js
+++ src/commands/dinner/index.js
@@ -1,6 +1,5 @@
 import { SlashCommandBuilder } from "discord.js";
-import { Dinner } from "../../../dinner.json";
-import { writeFileSync } from "fs";
+import { writeFileSync, readFileSync } from "fs";
 export const command = new SlashCommandBuilder()
   .setName("dinner")
   .setDescription("晚餐")
@@ -9,35 +8,73 @@ export const command = new SlashCommandBuilder()
       .setName("功能")
       .setDescription("選擇功能")
       .setRequired(true)
-      .addChoices({ name: "抽", value: "draw" }, { name: "新增", value: "add" })
+      .addChoices(
+        { name: "抽", value: "draw" },
+        { name: "新增", value: "add" },
+        { name: "刪除", value: "delete" },
+        { name: "全部", value: "all" }
+      )
   )
   .addStringOption((option) =>
-    option.setName("new").setDescription("newdinner")
+    option.setName("new").setDescription("new dinner")
+  )
+  .addStringOption((option) =>
+    option.setName("delete").setDescription("delete dinner")
   );
 
 export const action = async (ctx) => {
+  let data = readFileSync("dinner.json");
+  let dinner = JSON.parse(data);
   const dinnerChannel = await ctx.client.channels.fetch(
     process.env.DINNER_CHANNELID
   );
   const fun = ctx.options.getString("功能");
-  const dinner = getRandomDinner();
-  if (fun === "draw") {
-    await dinnerChannel.send(`${ctx.member.displayName} 抽到 ${dinner} `);
-    await ctx.reply({ content: "請到餐廳看結果", ephemeral: true });
-  } else if (fun === "add") {
-    const option = ctx.options.getString("new");
-    if (option) {
-      console.log(Dinner);
-      Dinner.push(option);
-      writeFileSync("dinner.json", JSON.stringify(Dinner));
-      await dinnerChannel.send(`${ctx.member.displayName} 新增了 ${option} `);
-      await ctx.reply({ content: "新增成功", ephemeral: true });
-    }
+
+  switch (fun) {
+    case "draw":
+      const chosenDinner = getRandomDinner(dinner);
+      await dinnerChannel.send(
+        `${ctx.member.displayName} 抽到 ${chosenDinner} `
+      );
+      await ctx.reply({ content: "請到餐廳看結果", ephemeral: true });
+      break;
+
+    case "add":
+      const option = ctx.options.getString("new");
+      if (option) {
+        dinner.push(option);
+        writeFileSync("dinner.json", JSON.stringify(dinner));
+        await dinnerChannel.send(`${ctx.member.displayName} 新增了 ${option} `);
+        await ctx.reply({ content: "新增成功", ephemeral: true });
+      } else {
+        await ctx.reply({ content: "新增失敗", ephemeral: true });
+      }
+      break;
+
+    case "delete":
+      const deleteOption = ctx.options.getString("delete");
+      if (dinner.includes(deleteOption)) {
+        const index = dinner.indexOf(deleteOption);
+        dinner.splice(index, 1);
+        writeFileSync("dinner.json", JSON.stringify(dinner));
+        await dinnerChannel.send(
+          `${ctx.member.displayName} 刪除了 ${deleteOption} `
+        );
+        await ctx.reply({ content: "刪除成功", ephemeral: true });
+      } else {
+        await ctx.reply({ content: "刪除失敗", ephemeral: true });
+      }
+      break;
+
+    case "all":
+      await dinnerChannel.send(`所有可能抽到的晚餐:${dinner}`);
+      await ctx.reply({ content: "請到餐廳看結果", ephemeral: true });
+      break;
   }
 };
 
-function getRandomDinner() {
-  const randomIndex = Math.floor(Math.random() * Dinner.length);
-  const dinner = Dinner[randomIndex];
-  return dinner;
+function getRandomDinner(dinner) {
+  const randomIndex = Math.floor(Math.random() * dinner.length);
+  const chosenDinner = dinner[randomIndex];
+  return chosenDinner;
 }
